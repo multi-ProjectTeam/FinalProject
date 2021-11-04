96c9115cde5e07d3101b0f096b47da8a8e327ad7
Hangyeol-Jang <8524hg@gmail.com>
2021-11-04 21:07:38 +0900

    project initialization

---
+++ src/main/java/com/example/finProject/FinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.finProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finProject/FinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
