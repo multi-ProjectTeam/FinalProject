6df28d1f444e26ac6d3520584d2e52cb4d0bc755
BISohn27 <skylord418@gmail.com>
2021-11-23 16:25:00 +0900

    save

--- src/main/java/com/example/finProject/elasticsearch/helper/Indices.java
+++ src/main/java/com/example/finProject/elasticsearch/helper/Indices.java
@@ -2,5 +2,5 @@ package com.example.finProject.elasticsearch.helper;
 
 //index의 이름을 변경할 경우 인덱스 이름을 사용하는 모든 클래스에서 변경하지 않고 여기서 변경하면 모든 이름이 변경되도록 만들어 주는 클래스
 public final class Indices {
-	public static final String BUSINESS_INDEX = "business";
+	public static final String ENTERPRISE_INDEX = "enterprise";
 }
